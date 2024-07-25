251d3cdb1aaf8a4d9ec72519af4748f9d1624b49
Tech <debitcardzpaypal@gmail.com>
2024-07-25 16:24:22 -0400

    feat: implement cache versioning

--- core/src/main/kotlin/gg/ingot/angostura/cache/KeyedCacheLayer.kt
+++ core/src/main/kotlin/gg/ingot/angostura/cache/KeyedCacheLayer.kt
@@ -8,13 +8,28 @@ import kotlin.time.Duration
  * `angostura:cache:account:<PROVIDED>`
  * @param T The type of the value to store.
  * @property key The key to store the value under.
+ * @property version The version of the cache.
  * @property ttl The time to live of the cached value.
  * @property refreshTTL If the time to live should be refreshed when the data is accessed.
  * @see CacheLayer
  * @since 1.0.0
  */
 abstract class KeyedCacheLayer<T : Any>(
-    protected val key: String,
+    private val key: String,
     ttl: Duration,
-    refreshTTL: Boolean = false
-) : CacheLayer<T>(ttl, refreshTTL)
+    refreshTTL: Boolean = false,
+    private val version: Int?
+) : CacheLayer<T>(ttl, refreshTTL) {
+    /**
+     * Build the key for the cache.
+     * @param str The identifier to build the key with.
+     * @return The built key.
+     */
+    protected fun buildKey(str: String): String {
+        return if (version != null) {
+            "$key:$version:$str"
+        } else {
+            "$key:$str"
+        }
+    }
+}
